9ec4e730096a937db0674102ba369f99ce96c4ec
kamilAndziak <74103819+kamilAndziak@users.noreply.github.com>
2020-11-07 15:51:52 +0100

    Update s03-fizzbuzz.cpp

--- s03-fizzbuzz.cpp
+++ s03-fizzbuzz.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 auto main(int argc, char *argv[]) -> int {
-    int i = std::stoi(argv[1]);
+    auto i = std::stoi(argv[1]);
     if (i%3 == 0){
         std::cout << "Fizz";
     }
