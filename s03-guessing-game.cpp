2084cd21ac4a8f151d1b7fbb84d6c78afea48769
kamilAndziak <74103819+kamilAndziak@users.noreply.github.com>
2020-11-07 15:46:46 +0100

    Update s03-guessing-game.cpp

--- s03-guessing-game.cpp
+++ s03-guessing-game.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <time.h>
 
 auto main() -> int {
-    int i = rand() % 100 + 1;
-    int in;
+    srand(time(NULL));
+    auto i = int(rand() % 100 + 1);
+    auto in = int();
+    auto line = std::string();
     while(true){
         std::cout << "guess: ";
-        std::cin >> in;
+        std::getline(std::cin, line);
+        in = std::stoi(line);
         if (in > i){
             std::cout << "number too big!" << std::endl;
             continue;
