67db829ef8deed6f3bb7b2b7abaf6741ce99f961
kamilAndziak <74103819+kamilAndziak@users.noreply.github.com>
2020-11-07 15:47:58 +0100

    Update s03-countdown.cpp

--- s03-countdown.cpp
+++ s03-countdown.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 auto main(int argc, char *argv[]) -> int {
-    for(int i = atoi(argv[1]); i >= 0; --i){
+    for(int i = std::stoi(argv[1]); i >= 0; --i){
         std::cout << i << std::endl;
     }
     return 0;
