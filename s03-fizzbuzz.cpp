7e22afd93f76dbcdebe74c76072bdb7c0e82c728
kamilAndziak <74103819+kamilAndziak@users.noreply.github.com>
2020-11-07 15:18:24 +0100

    s03-fizzbuzz.cpp

---
+++ s03-fizzbuzz.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+auto main(int argc, char *argv[]) -> int {
+    int i = atoi(argv[1]);
+    if (i%3 == 0){
+        std::cout << "Fizz";
+    }
+    if (i%5 == 0){
+        std::cout << "Buzz";
+    }
+    return 0;
+}
